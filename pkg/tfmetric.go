4be4324d2e23b26d8def32366d0f3c93069bde27
Jishnu <mohanjishnu@gmail.com>
2021-04-09 08:07:18 +0800

    Create function to extract metric values from config and payload

--- pkg/tfmetric.go
+++ pkg/tfmetric.go
@@ -1,6 +1,12 @@
 package tfmetric
 
 import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"strconv"
+	"strings"
+
 	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/metric"
 
@@ -9,13 +15,149 @@ import (
 
 type MetricInstance struct {
 	// The kind of metric to use.
-	metric_kind metric.InstrumentKind
+	metricKind metric.InstrumentKind
 	// The value of the metric.
-	metric_value interface{}
+	metricValue interface{}
 	// Sets of key-value pairs to use as labels.
 	labels []attribute.KeyValue
 }
 
-func CreateMetrics(config *configpb.MetricConfig, payload string) []MetricInstance {
-	return []MetricInstance{}
+type MetricContext int
+
+const (
+	UnknownContext MetricContext = iota
+	InputContext
+	OutputContext
+)
+
+func CreateMetrics(config *configpb.SidecarConfig, payload string, context MetricContext) []MetricInstance {
+	metricConfigs := []*configpb.MetricConfig{}
+	if context == InputContext {
+		metricConfigs = config.InputMetrics
+	}
+	if context == OutputContext {
+		metricConfigs = config.OutputMetrics
+	}
+
+	var jsonPayload interface{}
+	err := json.Unmarshal([]byte(payload), &jsonPayload)
+	if err != nil {
+		log.Fatal("Error when umarshaling payload json", err)
+	}
+
+	var output []MetricInstance
+	for _, config := range metricConfigs {
+		kind := getMetricKind(config)
+		value := getMetricValue(config, jsonPayload)
+		labels := getMetricLabels(config, jsonPayload)
+		output = append(output, MetricInstance{kind, value, labels})
+	}
+
+	return output
+}
+
+func getMetricKind(config *configpb.MetricConfig) metric.InstrumentKind {
+	if config.GetSimpleCounter() != nil {
+		return metric.CounterInstrumentKind
+	}
+	if config.GetValue() != nil {
+		return metric.ValueRecorderInstrumentKind
+	}
+	return -1
+}
+
+func getMetricValue(config *configpb.MetricConfig, jsonPayload interface{}) interface{} {
+	if config.GetSimpleCounter() != nil {
+		return 1
+	}
+	if config.GetValue() != nil {
+		value := config.GetValue().GetValue()
+		return extractValue(value, jsonPayload)
+	}
+	// Default to a counter
+	return 1
+}
+
+func getMetricLabels(config *configpb.MetricConfig, jsonPayload interface{}) []attribute.KeyValue {
+	var labels []attribute.KeyValue
+	for _, labelConfig := range config.Labels {
+		key := extractValue(labelConfig.LabelKey, jsonPayload)
+		value := extractValue(labelConfig.LabelValue, jsonPayload)
+
+		// The key must be a string.
+		if keyString, ok := key.(string); ok {
+			attributeKey := attribute.Key(keyString)
+			// We expect floats, ints, and strings only.
+			var keyValue attribute.KeyValue
+			switch value := value.(type) {
+			case string:
+				keyValue = attributeKey.String(value)
+			case int64:
+				keyValue = attributeKey.Int64(value)
+			case float64:
+				keyValue = attributeKey.Float64(value)
+			}
+			labels = append(labels, keyValue)
+		}
+	}
+	return labels
+}
+
+func extractValue(config *configpb.ValueConfig, jsonPayload interface{}) interface{} {
+	if config.GetParsedValue() != nil {
+		path := config.GetParsedValue().FieldPath.Paths[0]
+		pathSegments := strings.Split(path, ".")
+		valueType := config.GetParsedValue().ParsedType
+
+		payload := jsonPayload
+		for _, segment := range pathSegments {
+			if mapPayload, ok := payload.(map[string]interface{}); ok {
+				payload = mapPayload[segment]
+			}
+		}
+		switch valuePayload := payload.(type) {
+		case string:
+			switch valueType {
+			case configpb.ParsedValue_STRING:
+				return valuePayload
+			case configpb.ParsedValue_FLOAT:
+				val, err := strconv.ParseFloat(valuePayload, 64)
+				if err != nil {
+					log.Fatal("Error parsing float", err)
+				}
+				return val
+			case configpb.ParsedValue_INTEGER:
+				val, err := strconv.ParseInt(valuePayload, 10, 64)
+				if err != nil {
+					log.Fatal("Error parsing integer", err)
+				}
+				return val
+			}
+		case float64:
+			switch valueType {
+			case configpb.ParsedValue_FLOAT:
+				return valuePayload
+			case configpb.ParsedValue_INTEGER:
+				return int64(valuePayload)
+			case configpb.ParsedValue_STRING:
+				return fmt.Sprintf("%f", valuePayload)
+			}
+		default:
+			log.Fatal("Unexpected field when parsing JSON payload")
+		}
+	}
+
+	if config.GetStaticValue() != nil {
+		if config, ok := config.GetStaticValue().(*configpb.ValueConfig_StringValue); ok {
+			return config.StringValue
+		}
+		if config, ok := config.GetStaticValue().(*configpb.ValueConfig_IntegerValue); ok {
+			return config.IntegerValue
+		}
+		if config, ok := config.GetStaticValue().(*configpb.ValueConfig_FloatValue); ok {
+			return config.FloatValue
+		}
+	}
+
+	return nil
 }
--- pkg/tfmetric_test.go
+++ pkg/tfmetric_test.go
@@ -2,13 +2,156 @@ package tfmetric
 
 import (
 	"testing"
+
+	"go.opentelemetry.io/otel/metric"
+	"google.golang.org/protobuf/encoding/prototext"
+
+	configpb "github.com/metricrule-sidecar-tfserving/api/proto/metricconfigpb"
 )
 
 func TestInputCounter(t *testing.T) {
+	configTextProto := `
+		input_metrics {
+			simple_counter: {}
+		}`
+	var config configpb.SidecarConfig
+	prototext.Unmarshal([]byte(configTextProto), &config)
+
+	metrics := CreateMetrics(&config, "{}", InputContext)
+
+	gotLen := len(metrics)
+	wantLen := 1
+	if gotLen != wantLen {
+		t.Errorf("Unexpected length of metrics, got %v, wanted %v", gotLen, wantLen)
+	}
+
+	if gotLen == 0 {
+		return
+	}
+
+	gotFirstInstance := metrics[0]
+	gotKind := gotFirstInstance.metricKind
+	wantKind := metric.CounterInstrumentKind
+	if gotKind != wantKind {
+		t.Errorf("Unexpected metric kind, got %v, wanted %v", gotKind, wantKind)
+	}
+
+	gotValue := gotFirstInstance.metricValue
+	wantValue := 1
+	if gotValue != wantValue {
+		t.Errorf("Unexpected metric value, got %v, wanted %v", gotValue, wantValue)
+	}
+
+	gotLabelsLen := len(gotFirstInstance.labels)
+	wantLabelsLen := 0
+	if gotLabelsLen != wantLabelsLen {
+		t.Errorf("Unexpected labels length, got %v, wanted %v", gotLabelsLen, wantLabelsLen)
+	}
 }
 
 func TestInputCounterWithLabels(t *testing.T) {
+	configTextProto := `
+		input_metrics {
+			simple_counter: {}
+			labels: {
+				label_key: { string_value: "Application" }
+				label_value: { string_value: "MetricRule" }
+			}
+		}`
+	var config configpb.SidecarConfig
+	prototext.Unmarshal([]byte(configTextProto), &config)
+
+	metrics := CreateMetrics(&config, "{}", InputContext)
+
+	gotLen := len(metrics)
+	wantLen := 1
+	if gotLen != wantLen {
+		t.Errorf("Unexpected length of metrics, got %v, wanted %v", gotLen, wantLen)
+	}
+
+	if gotLen == 0 {
+		return
+	}
+
+	gotFirstInstance := metrics[0]
+	gotKind := gotFirstInstance.metricKind
+	wantKind := metric.CounterInstrumentKind
+	if gotKind != wantKind {
+		t.Errorf("Unexpected metric kind, got %v, wanted %v", gotKind, wantKind)
+	}
+
+	gotValue := gotFirstInstance.metricValue
+	wantValue := 1
+	if gotValue != wantValue {
+		t.Errorf("Unexpected metric value, got %v, wanted %v", gotValue, wantValue)
+	}
+
+	gotLabelsLen := len(gotFirstInstance.labels)
+	wantLabelsLen := 1
+	if gotLabelsLen != wantLabelsLen {
+		t.Errorf("Unexpected labels length, got %v, wanted %v", gotLabelsLen, wantLabelsLen)
+	}
+
+	if gotLabelsLen == 0 {
+		return
+	}
+
+	gotLabel := gotFirstInstance.labels[0]
+	wantLabelKey := "Application"
+	wantLabelValue := "MetricRule"
+	if string(gotLabel.Key) != wantLabelKey {
+		t.Errorf("Unexpected label key, got %v, wanted %v", gotLabel.Key, wantLabelKey)
+	}
+	if gotLabel.Value.AsString() != wantLabelValue {
+		t.Errorf("Unexpected label key, got %v, wanted %v", gotLabel.Value.AsString(), wantLabelValue)
+	}
 }
 
 func TestOutputValues(t *testing.T) {
+	configTextProto := `
+		output_metrics {
+			value {
+				value {
+					parsed_value {
+						field_path: {
+							paths: "prediction"
+						}
+						parsed_type: FLOAT
+					}
+				}
+			}
+		}`
+	var config configpb.SidecarConfig
+	prototext.Unmarshal([]byte(configTextProto), &config)
+
+	metrics := CreateMetrics(&config, "{ \"prediction\": 0.495 }", OutputContext)
+
+	gotLen := len(metrics)
+	wantLen := 1
+	if gotLen != wantLen {
+		t.Errorf("Unexpected length of metrics, got %v, wanted %v", gotLen, wantLen)
+	}
+
+	if gotLen == 0 {
+		return
+	}
+
+	gotFirstInstance := metrics[0]
+	gotKind := gotFirstInstance.metricKind
+	wantKind := metric.ValueRecorderInstrumentKind
+	if gotKind != wantKind {
+		t.Errorf("Unexpected metric kind, got %v, wanted %v", gotKind, wantKind)
+	}
+
+	gotValue := gotFirstInstance.metricValue
+	wantValue := 0.495
+	if gotValue != wantValue {
+		t.Errorf("Unexpected metric value, got %v, wanted %v", gotValue, wantValue)
+	}
+
+	gotLabelsLen := len(gotFirstInstance.labels)
+	wantLabelsLen := 0
+	if gotLabelsLen != wantLabelsLen {
+		t.Errorf("Unexpected labels length, got %v, wanted %v", gotLabelsLen, wantLabelsLen)
+	}
 }
