a21c9794be2fddd233b424a6890dfc2b4cdf6e3c
Jishnu <mohanjishnu@gmail.com>
2021-05-13 15:10:45 +0800

    Refactor out code that does request and response logging to be agnostic of HTTP Transport

--- pkg/mrtransport/mrtransport.go
+++ pkg/mrtransport/mrtransport.go
@@ -11,6 +11,7 @@ import (
 
 	configpb "github.com/metricrule-sidecar-tfserving/api/proto/metricconfigpb"
 	"github.com/metricrule-sidecar-tfserving/pkg/mrotel"
+	"github.com/metricrule-sidecar-tfserving/pkg/mrrecorder"
 	"github.com/metricrule-sidecar-tfserving/pkg/tfmetric"
 )
 
@@ -41,11 +42,16 @@ func (t *Transport) RoundTrip(req *http.Request) (*http.Response, error) {
 	if strings.Contains(req.Header.Get("Content-Type"), "json") {
 		dump, err := httputil.DumpRequest(req, true)
 		if err != nil {
-			return nil, err;
+			return nil, err
 		}
 
-		reqdata := requestLogData{dump, t.SidecarConfig, t.InInstrs, t.Meter}
-		go logRequestData(reqdata, ctxChan)
+		reqdata := mrrecorder.RequestLogData{
+			Dump:   dump,
+			Config: t.SidecarConfig,
+			Instrs: t.InInstrs,
+			Meter:  t.Meter,
+		}
+		go mrrecorder.LogRequestData(reqdata, ctxChan)
 	} else {
 		ctxChan <- []attribute.KeyValue{}
 	}
@@ -56,65 +62,19 @@ func (t *Transport) RoundTrip(req *http.Request) (*http.Response, error) {
 	}
 
 	if strings.Contains(res.Header.Get("Content-Type"), "json") {
-		dump, err := httputil.DumpResponse(res, true);
+		dump, err := httputil.DumpResponse(res, true)
 		if err != nil {
 			return res, err
 		}
 
-		resdata := responseLogData{dump, t.SidecarConfig, t.OutInstrs, t.Meter}
-		go logResponseData(resdata, ctxChan)
+		resdata := mrrecorder.ResponseLogData{
+			Dump:   dump,
+			Config: t.SidecarConfig,
+			Instrs: t.OutInstrs,
+			Meter:  t.Meter,
+		}
+		go mrrecorder.LogResponseData(resdata, ctxChan)
 	}
 
 	return res, nil
-}
-
-type requestLogData struct {
-	dump []byte
-	config *configpb.SidecarConfig
-	instrs map[tfmetric.MetricInstrumentSpec]mrotel.InstrumentWrapper
-	m MetricRecorder
-}
-
-// logRequestData logs metrics based on the input metrics,
-// and communicates context labels to the parameter channel.
-func logRequestData(d requestLogData, ctxChan chan<- []attribute.KeyValue) {
-	defer close(ctxChan)
-	
-	strdump := string(d.dump)
-	i := strings.Index(strdump, "{")
-	j := strdump[i:]
-	ctxLabels := tfmetric.GetContextLabels(d.config, j, tfmetric.InputContext)
-	metrics := tfmetric.GetMetricInstances(d.config, j, tfmetric.InputContext)
-	ctx := context.Background()
-	for spec, m := range metrics {
-		instr := d.instrs[spec]
-		v, err := instr.Record(m.MetricValue)
-		if err == nil {
-			d.m.RecordBatch(ctx, append(ctxLabels, m.Labels...), v)
-		}
-	}
-	ctxChan <- ctxLabels
-}
-
-type responseLogData struct {
-	dump []byte
-	config *configpb.SidecarConfig
-	instrs map[tfmetric.MetricInstrumentSpec]mrotel.InstrumentWrapper
-	m MetricRecorder
-}
-
-// logResponse logs metrics based on output data and labels in context.
-func logResponseData(d responseLogData, ctxChan <-chan []attribute.KeyValue) {
-	ctxLabels := <-ctxChan
-	strdump := string(d.dump)
-	j := strdump[strings.Index(strdump, "{"):]
-	metrics := tfmetric.GetMetricInstances(d.config, j, tfmetric.OutputContext)
-	ctx := context.Background()
-	for spec, m := range metrics {
-		instr := d.instrs[spec]
-		v, err := instr.Record(m.MetricValue)
-		if err == nil {
-			d.m.RecordBatch(ctx, append(ctxLabels, m.Labels...), v)
-		}
-	}
 }
